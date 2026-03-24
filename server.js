3dd8e87b5d1e8c028dd83594070fbdb199ffb6b6
ptopher <37601649+ptopher@users.noreply.github.com>
2026-03-24 01:28:26 -0400

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get('/voice', handleVoice);
 app.post('/voice', handleVoice);
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
