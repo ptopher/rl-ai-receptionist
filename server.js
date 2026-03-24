b7f528f81cbb24eec8f28e3ea8c8f86265316224
ptopher <37601649+ptopher@users.noreply.github.com>
2026-03-24 01:20:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,37 @@
 const express = require('express');
 const app = express();
-const VoiceResponse = require('twilio').twiml.VoiceResponse;
+
+const twilio = require('twilio');
+const VoiceResponse = twilio.twiml.VoiceResponse;
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
+// Root test
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
 
-function buildVoiceResponse() {
-  const twiml = new VoiceResponse();
-  twiml.say('Welcome to R L Small Engines. Please say your name.');
-  return twiml.toString();
+// SAFE voice handler
+function handleVoice(req, res) {
+  try {
+    const twiml = new VoiceResponse();
+
+    twiml.say('Welcome to R L Small Engines. Please say your name.');
+
+    res.set('Content-Type', 'text/xml');
+    res.send(twiml.toString());
+  } catch (err) {
+    console.error('VOICE ERROR:', err);
+    res.status(500).send('Error');
+  }
 }
 
-app.get('/voice', (req, res) => {
-  res.type('text/xml');
-  res.send(buildVoiceResponse());
-});
+// Support BOTH GET and POST
+app.get('/voice', handleVoice);
+app.post('/voice', handleVoice);
 
-app.post('/voice', (req, res) => {
-  res.type('text/xml');
-  res.send(buildVoiceResponse());
-});
-
+// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
